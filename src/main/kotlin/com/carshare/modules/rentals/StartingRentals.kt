7872a0a376a23e6953550e13cb86408b960dfbde
Marijn Huizendveld <marijn@huizendveld.com>
2026-04-08 17:31:20 +0200

    :package: Move into package for production code

---
+++ src/main/kotlin/com/carshare/modules/rentals/StartingRentals.kt
@@ -0,0 +1,36 @@
+package com.carshare.modules.rentals
+
+import com.carshare.infrastructure.messaging.Event
+import java.time.LocalDateTime
+
+data class RentalStarted(
+    /**
+     * @example "agreement:d77d8ff5-ab2f-401d-83dc-a6631ec87b38"
+     */
+    val agreementId: String,
+
+    /**
+     * @example "customer:92d68941-8783-471f-b3b8-7dbf7b803157"
+     */
+    val customerId: String,
+
+    /**
+     * @example "12598,2"
+     */
+    val odometerStart: String,
+
+    /**
+     * @example "52,35633° N, 4,83712° E"
+     */
+    val locationStart: String,
+
+    /**
+     * @example "2024-09-12 10:22 Europe/Amsterdam"
+     */
+    val rentalStarted: LocalDateTime,
+
+    /**
+     * @example "NL:GGS-10-N"
+     */
+    val vehicle: String
+): Event()
--- src/test/kotlin/com/carshare/modules/driving/UnlockVehicleTest.kt
+++ src/test/kotlin/com/carshare/modules/driving/UnlockVehicleTest.kt
@@ -4,40 +4,9 @@ import com.carshare.infrastructure.automation.ProcessManager
 import com.carshare.infrastructure.automation.testing.AutomationScenario
 import com.carshare.infrastructure.messaging.Command
 import com.carshare.infrastructure.messaging.Event
+import com.carshare.modules.rentals.RentalStarted
 import org.junit.jupiter.api.Test
 import java.time.LocalDateTime
-
-data class RentalStarted(
-    /**
-     * @example "agreement:d77d8ff5-ab2f-401d-83dc-a6631ec87b38"
-     */
-    val agreementId: String,
-
-    /**
-     * @example "customer:92d68941-8783-471f-b3b8-7dbf7b803157"
-     */
-    val customerId: String,
-
-    /**
-     * @example "12598,2"
-     */
-    val odometerStart: String,
-
-    /**
-     * @example "52,35633° N, 4,83712° E"
-     */
-    val locationStart: String,
-
-    /**
-     * @example "2024-09-12 10:22 Europe/Amsterdam"
-     */
-    val rentalStarted: LocalDateTime,
-
-    /**
-     * @example "NL:GGS-10-N"
-     */
-    val vehicle: String
-): Event()
 
 data class UnlockVehicle(
     /**
