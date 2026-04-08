d06e622b2d8aa26060e5099aece3a822c23bd170
Marijn Huizendveld <marijn@huizendveld.com>
2026-04-08 18:41:05 +0200

    :mag: `AvailableVehicles`
     - `It answers with availability when cars have been added to the fleet which have not been rented out`

--- src/test/kotlin/com/carshare/modules/findingVehicles/AvailableVehiclesTest.kt
+++ src/test/kotlin/com/carshare/modules/findingVehicles/AvailableVehiclesTest.kt
@@ -7,6 +7,15 @@ import com.carshare.infrastructure.messaging.Question
 import com.carshare.infrastructure.projection.Projection
 import com.carshare.infrastructure.projection.testing.ProjectionScenario
 import org.junit.jupiter.api.Test
+import java.time.LocalDateTime
+
+data class CarWasAddedToFleet (
+    val vehicle: String,
+    val parkedLocation: String,
+    val odometer: Double,
+    val vehicleClass: VehicleClass,
+    val addedToFleetAt: LocalDateTime
+): Event()
 
 data class WhatVehiclesAreAvailableInTheArea(
     val fleet: String,
@@ -26,6 +35,7 @@ data class AvailableVehicle(
 )
 
 class AvailableVehiclesInMemoryProjection: Projection {
+
     override fun acknowledge(event: Event) {
     }
 
@@ -42,6 +52,7 @@ class AvailableVehiclesInMemoryProjection: Projection {
 }
 
 class AvailableVehiclesTest {
+
     @Test
     fun `It answers with no availability when no cars have been added to the fleet` () {
         val scenario = ProjectionScenario()
@@ -56,6 +67,51 @@ class AvailableVehiclesTest {
                     "NL",
                     "52,37117° N, 4,87950° E",
                     listOf()
+                )
+            )
+            .assertOnProjection(AvailableVehiclesInMemoryProjection())
+    }
+    @Test
+    fun `It answers with availability when cars have been added to the fleet which have not been rented out` () {
+        val scenario = ProjectionScenario()
+            .given(
+                CarWasAddedToFleet(
+                    "NL:HZL-55-X",
+                    "52,34226° N, 4,87345° E",
+                    23.3,
+                    VehicleClass.FUN_PREMIUM,
+                    LocalDateTime.parse("2025-11-12T09:03:01")
+                ),
+                CarWasAddedToFleet(
+                    "NL:HZL-55-X",
+                    "52,34508° N, 4,84512° E",
+                    27.1,
+                    VehicleClass.FUN_PREMIUM,
+                    LocalDateTime.parse("2025-11-12T10:15:04")
+                )
+            )
+            .whenAskedFor(
+                WhatVehiclesAreAvailableInTheArea(
+                    "NL",
+                    "52,37117° N, 4,87950° E"
+                )
+            )
+            .thenExpect(
+                AvailableVehicles(
+                    "NL",
+                    "52,37117° N, 4,87950° E",
+                    listOf(
+                        AvailableVehicle(
+                            "NL:HTZ-11-G",
+                            "52,34226° N, 4,87345° E",
+                            VehicleClass.FUN_PREMIUM
+                        ),
+                        AvailableVehicle(
+                            "NL:HZL-55-X",
+                            "52,34508° N, 4,84512° E",
+                            VehicleClass.FUN_PREMIUM
+                        ),
+                    )
                 )
             )
             .assertOnProjection(AvailableVehiclesInMemoryProjection())
