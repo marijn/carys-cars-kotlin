fbe1135deb40996e2105e0806e751ea476948c8f
Marijn Huizendveld <marijn@huizendveld.com>
2026-04-08 18:13:35 +0200

    :white_check_mark: `AvailableVehicles`

--- src/test/kotlin/com/carshare/modules/findingVehicles/AvailableVehiclesTest.kt
+++ src/test/kotlin/com/carshare/modules/findingVehicles/AvailableVehiclesTest.kt
@@ -1,4 +1,60 @@
 package com.carshare.modules.findingVehicles
 
+import com.carshare.domain.VehicleClass
+import com.carshare.infrastructure.messaging.Answer
+import com.carshare.infrastructure.messaging.Event
+import com.carshare.infrastructure.messaging.Question
+import com.carshare.infrastructure.projection.Projection
+import com.carshare.infrastructure.projection.testing.ProjectionScenario
+import org.junit.jupiter.api.Test
+
+data class WhatVehiclesAreAvailableInTheArea(
+    val fleet: String,
+    val referenceLocation: String
+) : Question()
+
+data class AvailableVehicles(
+    val fleet: String,
+    val referenceLocation: String,
+    val availableVehicles: List<AvailableVehicle>
+): Answer()
+
+data class AvailableVehicle(
+    val vehicle: String,
+    val location: String,
+    val vehicleClass: VehicleClass
+)
+
+class AvailableVehiclesInMemoryProjection: Projection {
+    override fun acknowledge(event: Event) {
+    }
+
+    override fun ask(question: Question): Answer {
+        return AvailableVehicles(
+            "NL",
+            "52,37117° N, 4,87950° E",
+            listOf()
+        )
+    }
+}
+
 class AvailableVehiclesTest {
+    @Test
+    fun `It answers with no availability when no cars have been added to the fleet` () {
+        val scenario = ProjectionScenario()
+            .whenAskedFor(
+                WhatVehiclesAreAvailableInTheArea(
+                    "NL",
+                    "52,37117° N, 4,87950° E"
+                )
+            )
+            .thenExpect(
+                AvailableVehicles(
+                    "NL",
+                    "52,37117° N, 4,87950° E",
+                    listOf()
+                )
+            )
+            .assertOnProjection(AvailableVehiclesInMemoryProjection())
+    }
 }
