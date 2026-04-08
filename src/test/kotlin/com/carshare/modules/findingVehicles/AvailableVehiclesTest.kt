56885a7b6825c013403e20063d53f4f3567d9df6
Marijn Huizendveld <marijn@huizendveld.com>
2026-04-08 19:28:19 +0200

    :package: Move into package for production code.

--- src/test/kotlin/com/carshare/modules/findingVehicles/AvailableVehiclesTest.kt
+++ src/test/kotlin/com/carshare/modules/findingVehicles/AvailableVehiclesTest.kt
@@ -1,43 +1,12 @@
 package com.carshare.modules.findingVehicles
 
 import com.carshare.domain.VehicleClass
-import com.carshare.infrastructure.messaging.Answer
-import com.carshare.infrastructure.messaging.Event
-import com.carshare.infrastructure.messaging.Question
 import com.carshare.infrastructure.projection.Projection
 import com.carshare.infrastructure.projection.testing.ProjectionScenario
 import com.carshare.modules.fleetManagement.CarWasAddedToFleet
 import com.carshare.modules.fleetManagement.CarWasRemovedFromFleet
 import org.junit.jupiter.api.Test
 import java.time.LocalDateTime
-
-class ProjectionOfAvailableVehiclesIntoMemory: Projection {
-    private var vehicles = listOf<AvailableVehicle>()
-
-    override fun acknowledge(event: Event) {
-        when (event) {
-            is CarWasAddedToFleet -> vehicles = vehicles.plus(
-                AvailableVehicle(
-                    event.vehicle,
-                    event.parkedLocation,
-                    event.vehicleClass
-                )
-            )
-            is CarWasRemovedFromFleet -> vehicles = vehicles.dropWhile { vehicle -> vehicle.vehicle == event.vehicle }
-        }
-    }
-
-    override fun ask(question: Question): Answer {
-        return when(question) {
-            is WhatVehiclesAreAvailableInTheArea -> AvailableVehicles(
-                question.fleet,
-                question.referenceLocation,
-                vehicles
-            )
-            else -> throw Exception("Unknown query")
-        }
-    }
-}
 
 abstract class AvailableVehiclesTest {
 
