ccf49d6d7f260933328b14e53c2385b9ac496698
Marijn Huizendveld <marijn@huizendveld.com>
2026-04-14 07:41:50 +0200

    :mag: `ReservingStateLoaderTest`
     - `Falls back to provided default state in absence of state`

---
+++ src/test/kotlin/com/carshare/modules/reserving/ReservingStateLoaderTest.kt
@@ -0,0 +1,50 @@
+package com.carshare.modules.reserving
+
+import com.carshare.infrastructure.decider.State
+import com.carshare.infrastructure.messaging.Event
+import com.carshare.modules.LicensePlate
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+
+class InMemoryReservingStateLoaderTest: ReservingStateLoaderTest() {
+    override fun createSubjectUnderTest(): StateLoader<LicensePlate, ReservingState, AnyReservationEvent> {
+        return InMemoryReservingStateLoader()
+    }
+}
+
+abstract class ReservingStateLoaderTest {
+    @Test()
+    fun `Falls back to provided default state in absence of state`() {
+        // arrange
+        val stateLoader = createSubjectUnderTest();
+
+        // act
+        val fallback = ReservingState.VehicleIsUnavailable()
+        val actual = stateLoader.loadStateOrElse(
+            LicensePlate.DutchLicensePlate("JTG-59-R"),
+            fallback
+        );
+
+        // assert
+        val expected = fallback;
+        Assertions.assertThat(actual).usingRecursiveComparison().isEqualTo(expected)
+    }
+
+    abstract fun createSubjectUnderTest(): StateLoader<LicensePlate, ReservingState, AnyReservationEvent>
+}
+
+interface StateLoader<AnyStateId, AnyState: State<AnyEvent>, AnyEvent: Event> {
+    fun add(identifiedBy: AnyStateId, state: AnyState): Void
+
+    fun loadStateOrElse(identifiedBy: AnyStateId, fallback: AnyState): AnyState
+}
+
+class InMemoryReservingStateLoader: StateLoader<LicensePlate, ReservingState, AnyReservationEvent> {
+    override fun add(identifiedBy: LicensePlate, state: ReservingState): Void {
+        TODO("Not yet implemented")
+    }
+
+    override fun loadStateOrElse(identifiedBy: LicensePlate, fallback: ReservingState): ReservingState {
+        TODO("Not yet implemented")
+    }
+}
