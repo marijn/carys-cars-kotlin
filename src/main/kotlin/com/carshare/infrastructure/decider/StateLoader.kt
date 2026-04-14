8077d0d1794b5e8151e15f483a34921782e24979
Marijn Huizendveld <marijn@huizendveld.com>
2026-04-14 11:47:58 +0200

    :memo: Log implementation improvement.

--- src/main/kotlin/com/carshare/infrastructure/decider/StateLoader.kt
+++ src/main/kotlin/com/carshare/infrastructure/decider/StateLoader.kt
@@ -3,7 +3,9 @@ package com.carshare.infrastructure.decider
 import com.carshare.infrastructure.messaging.Event
 
 interface StateLoader<AnyStateId, AnyState: State<AnyEvent>, AnyEvent: Event> {
+    // FIXME: suspend
     fun add(identifiedBy: AnyStateId, state: AnyState)
 
+    // FIXME: suspend
     fun loadStateOrElse(identifiedBy: AnyStateId, fallback: AnyState): AnyState
 }
