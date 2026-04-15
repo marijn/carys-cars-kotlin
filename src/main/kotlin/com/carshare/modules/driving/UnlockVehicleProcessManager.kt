3af76659db31a732c8dd4f349d277164822670c3
Marijn Huizendveld <marijn@huizendveld.com>
2026-04-15 10:24:03 +0200

    :white_check_mark: Constrain types.

--- src/main/kotlin/com/carshare/modules/driving/UnlockVehicleProcessManager.kt
+++ src/main/kotlin/com/carshare/modules/driving/UnlockVehicleProcessManager.kt
@@ -2,15 +2,14 @@ package com.carshare.modules.driving
 
 import com.carshare.infrastructure.automation.ProcessManager
 import com.carshare.infrastructure.messaging.Command
-import com.carshare.infrastructure.messaging.Event
+import com.carshare.modules.AnyUnlockVehicleAutomationEvent
 import com.carshare.modules.UnlockVehicle
 import com.carshare.modules.RentalStarted
 
-class UnlockVehicleProcessManager: ProcessManager<Event, Command> {
-    override fun processEvent(trigger: Event): List<Command> {
+class UnlockVehicleProcessManager: ProcessManager<AnyUnlockVehicleAutomationEvent, Command> {
+    override fun processEvent(trigger: AnyUnlockVehicleAutomationEvent): List<Command> {
         return when(trigger) {
             is RentalStarted -> unlockVehicle(trigger.vehicle)
-            else -> listOf()
         }
     }
 
