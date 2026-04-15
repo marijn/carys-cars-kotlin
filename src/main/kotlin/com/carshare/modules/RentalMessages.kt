d8a3ed46c70bf2b79255b8eb1a454918719da6d8
Marijn Huizendveld <marijn@huizendveld.com>
2026-04-15 09:48:56 +0200

    :eyeglasses: Follow coding standard.

--- src/main/kotlin/com/carshare/modules/RentalMessages.kt
+++ src/main/kotlin/com/carshare/modules/RentalMessages.kt
@@ -3,7 +3,7 @@ package com.carshare.modules
 import java.time.LocalDateTime
 
 data class RentalEnded (
-    public val rentalId: String,
-    public val customerId: String,
-    public val rentalStarted: LocalDateTime
+    val rentalId: String,
+    val customerId: String,
+    val rentalStarted: LocalDateTime
 ): TrialAutomationEvent, TrialProjectEvents
